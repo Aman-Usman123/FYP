e963e415b8e0347fde800e0adc267010a4f7e95a
Aman-Usman123 <120277244+Aman-Usman123@users.noreply.github.com>
2022-12-11 20:31:36 +0500

    hellpo

--- app/src/main/java/com/example/bidapp/buyingoption.kt
+++ app/src/main/java/com/example/bidapp/buyingoption.kt
@@ -2,6 +2,7 @@ package com.example.bidapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,6 +28,8 @@ class buyingoption : Fragment() {
        val v= inflater.inflate(R.layout.fragment_buyingoption, container, false)
       val  switch2=v.findViewById<TextView>(R.id.switch2)
         switch2.setOnClickListener{
+
+            Log.d("asd", "onCreateView: ")
             val sellingoptionoption=Sellingoption()
             val transaction: FragmentTransaction =parentFragmentManager!!.beginTransaction()
             transaction.replace(R.id.mainlayout,sellingoptionoption).commit()
